ac85e74983adddad098f798f5e5deabeaf139747
Amin Vakil <info@aminvakil.com>
2020-07-31 21:22:22 +0430

    Fix

--- travis/jmeter-plugins-manager/before_makepkg.sh
+++ travis/jmeter-plugins-manager/before_makepkg.sh
@@ -2,4 +2,4 @@
 pacman -Suy git --noconfirm
 git clone https://aur.archlinux.org/jmeter.git
 chown -R devel: jmeter
-su devel sh -c "cd jmeter && makepkg -sri --confirm"
+su devel sh -c "cd jmeter && makepkg -sri --noconfirm"
