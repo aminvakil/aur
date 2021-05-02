f545ec686f50aac0fbf1be5dc079f9e0862d9102
Amin Vakil <info@aminvakil.com>
2021-05-03 00:56:28 +0430

    remove libgmp-static from makedepends

--- ci/hadolint-git/before_makepkg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pacman -Suy git --noconfirm
-git clone https://aur.archlinux.org/libgmp-static.git
-chown -R devel: libgmp-static
-su devel sh -c "cd libgmp-static && makepkg -sri --noconfirm"
