3672e7df512cead3c6354f9ed1c693ef2c658c82
Amin Vakil <info@aminvakil.com>
2022-01-10 10:47:17 +0330

    Add python-spotdl ci

---
+++ ci/python-spotdl/before_makepkg.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pacman -Syu git --noconfirm
+for i in {python-spotipy,python-pytube,python-rapidfuzz,python-ytmusicapi}; do
+        git clone https://aur.archlinux.org/$i.git
+        chown -R devel: $i
+        su devel sh -c "cd $i && makepkg -sri --noconfirm"
+done;
