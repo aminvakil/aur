8ed252f02fed14d6480ec8fc5385c4e618d97308
Amin Vakil <info@aminvakil.com>
2022-02-12 00:54:49 +0330

    Fix pacman.conf path

--- ci/hadolint-git/before_makepkg.sh
+++ ci/hadolint-git/before_makepkg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sed -i "/\[testing\]/,/Include/"'s/^#//' pacman.conf
-sed -i "/\[community-testing\]/,/Include/"'s/^#//' pacman.conf
+sed -i "/\[testing\]/,/Include/"'s/^#//' /etc/pacman.conf
+sed -i "/\[community-testing\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Syu --noconfirm
