8760c052b5e1a79d3f969b326524762a26b9eb9f
Amin Vakil <info@aminvakil.com>
2022-03-14 22:19:30 +0330

    Also install python-jarowinkler before

--- ci/python-spotdl/before_makepkg.sh
+++ ci/python-spotdl/before_makepkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 pacman -Syu git --noconfirm
-for i in {python-spotipy,python-pytube,python-rapidfuzz,python-ytmusicapi}; do
+for i in {python-jarowinkler,python-spotipy,python-pytube,python-rapidfuzz,python-ytmusicapi}; do
         git clone "https://aur.archlinux.org/$i.git"
         chown -R devel: "$i"
         su devel sh -c "cd $i && makepkg -sri --noconfirm"
