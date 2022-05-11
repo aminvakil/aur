abd31591e8fdf084cf9cc173c52ab959a96feb01
Amin Vakil <info@aminvakil.com>
2022-05-11 10:27:02 +0430

    Also check for ansible-lint run after_makepkg
    Make sure git is installed
    
    cd and ansible-lint .

--- ci/ansible-lint-git/after_makepkg.sh
+++ ci/ansible-lint-git/after_makepkg.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -eux
 ansible-lint --version
+PROJECT_NAME="ansible-role-docker-installation"
+REPO="https://github.com/aminvakil/${PROJECT_NAME}"
+sudo pacman -Syu git --noconfirm
+git clone "${REPO}"
+cd "${PROJECT_NAME}"
+ansible-lint .
+cd ..
+rm -rf "${PROJECT_NAME}"
