99db8ae84ec855073f28a11259adf0fc7beb8f7b
Amin Vakil <info@aminvakil.com>
2022-05-14 14:40:20 +0430

    makepkg cpp-taskflow before python-rapidfuzz
    Needed for python-spotdl before_makepkg

--- ci/python-spotdl/before_makepkg.sh
+++ ci/python-spotdl/before_makepkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 pacman -Syu git --noconfirm
-for i in {python-rapidfuzz-capi,python-jarowinkler,python-spotipy,python-pytube,rapidfuzz-cpp,jarowinkler-cpp,python-rapidfuzz,python-ytmusicapi}; do
+for i in {cpp-taskflow,python-rapidfuzz-capi,python-jarowinkler,python-spotipy,python-pytube,rapidfuzz-cpp,jarowinkler-cpp,python-rapidfuzz,python-ytmusicapi}; do
         git clone "https://aur.archlinux.org/$i.git"
         chown -R devel: "$i"
         su devel sh -c "cd $i && makepkg -sri --noconfirm"
