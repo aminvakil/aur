82d1d4cc654b7e5ae6d652d0efe1e38d7dfedf7a
Amin Vakil <info@aminvakil.com>
2022-06-25 16:50:34 +0430

    Revert "Merge pull request #147 from aminvakil/hadolint_git_en_US"
    This reverts commit fc4b2b1b484d3b64d05ed19140c78f1ca4ac5c79, reversing
    changes made to d7c80b10549c6e88992ffa5bda368b6d255174c4.

--- ci/hadolint-git/before_makepkg.sh
+++ ci/hadolint-git/before_makepkg.sh
@@ -3,4 +3,3 @@ set -eux
 sed -i "/\[testing\]/,/Include/"'s/^#//' /etc/pacman.conf
 sed -i "/\[community-testing\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Syu --noconfirm
-localectl set-locale LANG=en_US.UTF-8
