0df867ced5eaaef6b94b3f22c0888978c49eda74
Amin Vakil <info@aminvakil.com>
2022-09-02 15:22:50 +0430

    Revert "Temporary fix for getfedora.org incorrect headers"
    This reverts commit 70d99b6f8bf2a3a96d125f58df9450c5ea5e2843.

--- ci/butane-bin/before_makepkg.sh
+++ ci/butane-bin/before_makepkg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eux
 pacman -Suy gnupg curl --noconfirm
-su devel sh -c "curl --http1.1 https://getfedora.org/static/fedora.gpg | gpg --import"
+su devel sh -c "curl https://getfedora.org/static/fedora.gpg | gpg --import"
