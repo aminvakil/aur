26d0fcd255931b76bc9ea0a3bffd283e16c47c99
Amin Vakil <info@aminvakil.com>
2022-09-24 17:47:09 +0330

    Deleted package: xssmap
    Remove xssmap

--- ci/xssmap/before_makepkg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eux
-pacman -Suy git --noconfirm
-git clone https://aur.archlinux.org/phantomjs.git
-chown -R devel: phantomjs
-su devel sh -c "cd phantomjs && makepkg -sri --noconfirm"
