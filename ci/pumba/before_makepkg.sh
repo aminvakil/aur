5246d481fcbbb837f0effa8740ea11d7307edc9d
Amin Vakil <info@aminvakil.com>
2023-06-03 13:54:11 +0330

    Remove pumba testing section

--- ci/pumba/before_makepkg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eux
-pacman -Syu git --noconfirm
-git clone https://aur.archlinux.org/golangci-lint.git
-chown -R devel: golangci-lint
-su devel sh -c "cd golangci-lint && makepkg -sri --noconfirm"
