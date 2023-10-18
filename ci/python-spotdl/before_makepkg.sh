04adba466502fab9e7b839246afee9539b3324cb
Amin Vakil <info@aminvakil.com>
2023-10-18 13:33:43 +0330

    Disable check in python-spotdl before_makepkg

--- ci/python-spotdl/before_makepkg.sh
+++ ci/python-spotdl/before_makepkg.sh
@@ -4,5 +4,5 @@ pacman -Syu git --noconfirm
 for i in {python-spotipy,python-pytube,python-pox,python-klepto,python-jaconv,python-pykakasi,python-syncedlyrics,python-bandcamp-api-git,python-demjson3,python-dacite,python-soundcloud-v2}; do
         git clone "https://aur.archlinux.org/$i.git"
         chown -R devel: "$i"
-        su devel sh -c "cd $i && makepkg -sri --noconfirm"
+        su devel sh -c "cd $i && makepkg -sri --nocheck --noconfirm"
 done;
