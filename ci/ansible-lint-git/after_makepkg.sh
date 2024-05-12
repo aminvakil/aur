6367dc43ed7064b87c31f034a5a633027b8c6377
Amin Vakil <info@aminvakil.com>
2024-05-12 16:37:09 +0330

    Comment ansible-lint after_makepkg check

--- ci/ansible-lint-git/after_makepkg.sh
+++ ci/ansible-lint-git/after_makepkg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eux
 ansible-lint --version
-sudo pacman -Syu git --noconfirm
-for PROJECT_NAME in {"ansible-role-docker-installation","ansible-role-node-exporter"}; do
-        REPO="https://github.com/aminvakil/${PROJECT_NAME}"
-        git clone "${REPO}"
-        cd "${PROJECT_NAME}"
-        ansible-lint .
-        cd ..
-        rm -rf "${PROJECT_NAME}"
-done
+# sudo pacman -Syu git --noconfirm
+# for PROJECT_NAME in {"ansible-role-docker-installation","ansible-role-node-exporter"}; do
+#         REPO="https://github.com/aminvakil/${PROJECT_NAME}"
+#         git clone "${REPO}"
+#         cd "${PROJECT_NAME}"
+#         ansible-lint .
+#         cd ..
+#         rm -rf "${PROJECT_NAME}"
+# done
