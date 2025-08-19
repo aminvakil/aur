074a77867cb2b24e468a89f952a18c3d0389e866
Amin Vakil <info@aminvakil.com>
2025-08-19 13:11:31 +0330

    Use github mirror of aur in ansible-cmdb before_makepkg

--- ci/ansible-cmdb/before_makepkg.sh
+++ ci/ansible-cmdb/before_makepkg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
 pacman -Suy git --noconfirm
-git clone https://aur.archlinux.org/python-jsonxs.git
+git clone --branch python-jsonxs --single-branch https://github.com/archlinux/aur/ python-jsonxs
 chown -R devel: python-jsonxs
 su devel sh -c "cd python-jsonxs && makepkg -sri --noconfirm"
