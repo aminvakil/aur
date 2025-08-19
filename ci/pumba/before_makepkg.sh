cef2109eed7fde2ae068c7606b54ed0c0618b2a0
Amin Vakil <info@aminvakil.com>
2025-08-19 12:32:36 +0330

    Use github mirror of aur in pumba before_makepkg

--- ci/pumba/before_makepkg.sh
+++ ci/pumba/before_makepkg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
 pacman -Syu git --noconfirm
-git clone https://aur.archlinux.org/golangci-lint.git
+git clone --branch golangci-lint --single-branch https://github.com/archlinux/aur/ golangci-lint
 chown -R devel: golangci-lint
 su devel sh -c "cd golangci-lint && makepkg -sri --noconfirm"
