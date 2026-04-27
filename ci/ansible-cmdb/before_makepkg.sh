5fa696e6e395796d88e5297e0a12a0ffe91b6a54
Amin Vakil <info@aminvakil.com>
2026-04-27 17:21:35 +0330

    s/Suy/Syu/g

--- ci/ansible-cmdb/before_makepkg.sh
+++ ci/ansible-cmdb/before_makepkg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
-pacman -Suy git --noconfirm
+pacman -Syu git --noconfirm
 git clone --branch python-jsonxs --single-branch https://github.com/archlinux/aur/ python-jsonxs
 chown -R devel: python-jsonxs
 sudo -u devel sh -c "cd python-jsonxs && makepkg -sri --noconfirm"
--- ci/jmeter-plugins-manager/before_makepkg.sh
+++ ci/jmeter-plugins-manager/before_makepkg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
-pacman -Suy git --noconfirm
+pacman -Syu git --noconfirm
 git clone --branch jmeter --single-branch https://github.com/archlinux/aur/ jmeter
 chown -R devel: jmeter
 sudo -u devel sh -c "cd jmeter && makepkg -sri --noconfirm"
