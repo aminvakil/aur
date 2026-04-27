f55fa039bcea157a1f383c1982fa3ae8aa27cd25
Amin Vakil <info@aminvakil.com>
2026-04-27 17:12:46 +0330

    Remove stale ci hooks

--- ci/actionlint/after_makepkg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Eeuox pipefail
-ACTIONLINT_VERSION=$(actionlint --version | head -n 1)
-PKGVER=$(grep -E "^pkgver=" /pkg/PKGBUILD | cut -d "=" -f 2)
-if [ "${ACTIONLINT_VERSION}" -ne "${PKGVER}" ]; then
-	echo "${ACTIONLINT_VERSION}"
-	echo "${PKGVER}"
-        exit 1
-fi
--- ci/fcct-bin/before_makepkg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eux
-pacman -Suy gnupg curl --noconfirm
-sudo -u devel sh -c "curl https://getfedora.org/static/fedora.gpg | gpg --import"
