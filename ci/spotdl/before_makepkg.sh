3300e3c0bfb2d2d94d581ac74850ca3ab2658e96
Amin Vakil <info@aminvakil.com>
2026-04-29 16:20:05 +0330

    Remove unnecessary pkgs from spotdl before_makepkg

--- ci/spotdl/before_makepkg.sh
+++ ci/spotdl/before_makepkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 pacman -Syu git --noconfirm
-for i in {python-spotipy,python-pox,python-klepto,python-syncedlyrics,python-bandcamp-api-git,python-demjson3,python-dacite,python-soundcloud-v2,python-datastar-py}; do
+for i in {python-spotipy,python-syncedlyrics,python-dacite,python-soundcloud-v2,python-datastar-py}; do
         git clone --branch "${i}" --single-branch https://github.com/archlinux/aur/ "${i}"
         chown -R devel: "$i"
         sudo -u devel sh -c "cd $i && makepkg -sri --nocheck --noconfirm"
