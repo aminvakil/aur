ad3de4699e5eb6dbf1b255071d66e73e9f29e306
Amin Vakil <info@aminvakil.com>
2026-05-16 13:24:02 +0330

    Add python-tls-client-git before_makepkg

---
+++ ci/python-tls-client-git/before_makepkg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eux
+pacman -Syu git --noconfirm
+for i in {tls-client-git}; do
+        git clone --branch "${i}" --single-branch https://github.com/archlinux/aur/ "${i}"
+        chown -R devel: "$i"
+        sudo -u devel sh -c "cd $i && makepkg -sri --nocheck --noconfirm"
+done;
