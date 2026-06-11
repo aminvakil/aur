1c7dfc0bde282846bb467463575224b152e91b56
Amin Vakil <info@aminvakil.com>
2026-06-11 21:38:27 +0330

    Install loreai before loreai-pi

---
+++ ci/loreai-pi/before_makepkg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eux
+pacman -Syu git --noconfirm
+i="loreai"
+git clone --branch "${i}" --single-branch https://github.com/archlinux/aur/ "${i}"
+chown -R devel: "$i"
+sudo -u devel sh -c "cd $i && makepkg -sri --nocheck --noconfirm"
